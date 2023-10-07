473a3865030fea3be929d4656e328f33a95181ce
Dai <dainpse150682@fpt.edu.vn>
2023-10-07 19:49:01 +0700

    staff table

---
+++ src/layout/LayoutAdmin/Topbar/Topbar.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./Topbar.scss"
+
+const Topbar = () => {
+    return (
+        <div className="top-bar">
+            <div className="logo">Orders</div>
+
+            <div className="notif"></div>
+        </div>
+    )
+}
+
+export default Topbar
---
+++ src/pages/admin/StaffList.jsx/Menu.jsx
@@ -0,0 +1,120 @@
+import * as React from "react"
+import Box from "@mui/material/Box"
+import Tab from "@mui/material/Tab"
+import TabContext from "@mui/lab/TabContext"
+import TabList from "@mui/lab/TabList"
+import TabPanel from "@mui/lab/TabPanel"
+import Searchbar from "../../../layout/LayoutAdmin/Topbar/Searchbar"
+
+import {
+    Paper,
+    Table,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+} from "@mui/material"
+
+export default function StaffTable() {
+    const [value, setValue] = React.useState("1")
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue)
+    }
+
+    return (
+        <Box sx={{ width: "100%", typography: "body1" }}>
+            <TabContext value={value}>
+                <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                    <TabList
+                        onChange={handleChange}
+                        aria-label="lab API tabs example"
+                    >
+                        <Tab label="Passed" value="passed" />
+                        <Tab label="Rejected" value="rejected" />
+                    </TabList>
+                </Box>
+                <TabPanel value="passed">
+                    <TableContainer component={Paper}>
+                        <div className="search-bar">
+                            <Searchbar />
+                        </div>
+                        <Table
+                            sx={{ minWidth: 450, width: "100%" }}
+                            aria-label="simple table"
+                        >
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell
+                                        sx={{
+                                            fontWeight: "bolder",
+                                            fontSize: "1rem",
+                                        }}
+                                    >
+                                        Order
+                                    </TableCell>
+                                    <TableCell
+                                        sx={{
+                                            fontWeight: "bolder",
+                                            fontSize: "1rem",
+                                        }}
+                                        align="center"
+                                    >
+                                        Date
+                                    </TableCell>
+                                    <TableCell
+                                        sx={{
+                                            fontWeight: "bolder",
+                                            fontSize: "1rem",
+                                        }}
+                                        align="center"
+                                    >
+                                        Customer
+                                    </TableCell>
+                                    <TableCell
+                                        sx={{
+                                            fontWeight: "bolder",
+                                            fontSize: "1rem",
+                                        }}
+                                        align="center"
+                                    >
+                                        Payment
+                                    </TableCell>
+                                    <TableCell
+                                        sx={{
+                                            fontWeight: "bolder",
+                                            fontSize: "1rem",
+                                        }}
+                                        align="center"
+                                    >
+                                        Status
+                                    </TableCell>
+                                    <TableCell
+                                        sx={{
+                                            fontWeight: "bolder",
+                                            fontSize: "1rem",
+                                        }}
+                                        align="center"
+                                    >
+                                        Price
+                                    </TableCell>
+                                </TableRow>
+                            </TableHead>
+                        </Table>
+
+                        <div
+                            className="pagination"
+                            style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                marginTop: "1rem",
+                                marginLeft: "auto",
+                            }}
+                        ></div>
+                    </TableContainer>
+                </TabPanel>
+                <TabPanel value="rejected">ss</TabPanel>
+            </TabContext>
+        </Box>
+    )
+}
