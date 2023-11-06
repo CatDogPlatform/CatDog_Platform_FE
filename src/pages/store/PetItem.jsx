063540fed72a1e2a8debc54c00b1432f459c5b3b
Dai <dainpse150682@fpt.edu.vn>
2023-11-06 15:21:44 +0700

    code

--- src/pages/store/PetItem.jsx
+++ src/pages/store/PetItem.jsx
@@ -5,54 +5,87 @@ import { useState } from "react";
 import { DetailModal } from "./DetailModal";
 import axios from "axios";
 function PetItem() {
-    const [pets, setPets] = React.useState([]);
+  const [pets, setPets] = React.useState([]);
 
-    const fetchPets = async () => {
-        const res = await axios.get("https://petdom-apis.onrender.com/api/pets?search=");
-        console.log(res);
-        setPets(res.data);
-    };
-
-    React.useEffect(() => {
-        fetchPets();
-    }, []);
-
-    const [isDetailModalOpen, setIsDetailModalOpen] = useState(false);
-    const [selectedCandidate, setSelectedCandidate] = useState(null);
-    const handleCloseDetailModal = () => {
-        setIsDetailModalOpen(false);
-    };
-
-    const handleDetailClick = (item) => {
-        setSelectedCandidate(item);
-        setIsDetailModalOpen(true);
-    };
-    return (
-        <div
-            className="StoreItem"
-            style={{
-                display: "grid",
-                gridTemplateRows: "repeat(4, minmax(0, 1fr))",
-                columnGap: "1.5rem",
-                gridTemplateColumns: "repeat(4, minmax(0, 1fr))",
-            }}
-        >
-            {pets &&
-                pets.map((item) => (
-                    <div style={{ margin: "10px 0 50px 0" }}>
-                        <img src={item.images} alt="" />
-                        <p className="item-name" style={{ cursor: "pointer" }} onClick={() => handleDetailClick(item)}>
-                            {item.name}
-                        </p>
-                        <p className="item-name" style={{ color: "#0F60DA" }}>
-                            {item.price}
-                        </p>
-                        <span style={{ fontSize: "12px", color: "#888484" }}>{item?.user?.fullname}</span>
-                    </div>
-                ))}
-            <DetailModal isOpen={isDetailModalOpen} handleCloseDetailModal={handleCloseDetailModal} item={selectedCandidate} />
-        </div>
+  const fetchPets = async () => {
+    const res = await axios.get(
+      "https://petdom-apis.onrender.com/api/pets?search="
     );
+    console.log(res);
+    setPets(res.data);
+  };
+
+  React.useEffect(() => {
+    fetchPets();
+  }, []);
+
+  const [isDetailModalOpen, setIsDetailModalOpen] = useState(false);
+  const [selectedCandidate, setSelectedCandidate] = useState(null);
+  const handleCloseDetailModal = () => {
+    setIsDetailModalOpen(false);
+  };
+
+  const handleDetailClick = (item) => {
+    setSelectedCandidate(item);
+    setIsDetailModalOpen(true);
+  };
+  return (
+    <div
+      className="StoreItem"
+      style={{
+        display: "grid",
+        gridTemplateRows: "repeat(4, minmax(0, 1fr))",
+        columnGap: "1.5rem",
+        gridTemplateColumns: "repeat(4, minmax(0, 1fr))",
+      }}
+    >
+      {pets &&
+        pets.map((item) => (
+          <div style={{ margin: "10px 0 50px 0" }}>
+            <img
+              className="item-image"
+              style={{
+                border: "1px solid wheat",
+                borderRadius: "8px",
+                width: "200px",
+                height: "178px",
+              }}
+              src={item.images}
+              alt=""
+            />
+            <p
+              className="item-name"
+              style={{
+                cursor: "pointer",
+                display: "flex",
+                // alignItems: "center",
+                // justifyContent: "center",
+                // margin: "15px 55px 10px 15px",
+                fontSize: "17px",
+                fontWeight: "800",
+              }}
+              onClick={() => handleDetailClick(item)}
+            >
+              {item.name}
+            </p>
+            <p
+              className="item-name"
+              style={{ color: "#0F60DA", fontSize: "15px", fontWeight: "600" }}
+            >
+              {item.price}
+            </p>
+            <span style={{ fontSize: "12px", color: "#888484" }}>
+              {item?.user?.fullname}
+            </span>
+          </div>
+        ))}
+      <DetailModal
+        isOpen={isDetailModalOpen}
+        handleCloseDetailModal={handleCloseDetailModal}
+        item={selectedCandidate}
+      />
+    </div>
+  );
 }
 
 export default PetItem;
--- src/pages/store/StoreItem.jsx
+++ src/pages/store/StoreItem.jsx
@@ -4,52 +4,81 @@ import { ItemList } from "./ItemList";
 import axios from "axios";
 import { GoodDetailModal } from "./GoodDetailModal";
 function StoreItem() {
-    const [goods, setGoods] = React.useState([]);
-    const [isDetailModalOpen, setIsDetailModalOpen] = useState(false);
-    const [selectedCandidate, setSelectedCandidate] = useState(null);
-    const handleCloseDetailModal = () => {
-        setIsDetailModalOpen(false);
-    };
+  const [goods, setGoods] = React.useState([]);
+  const [isDetailModalOpen, setIsDetailModalOpen] = useState(false);
+  const [selectedCandidate, setSelectedCandidate] = useState(null);
+  const handleCloseDetailModal = () => {
+    setIsDetailModalOpen(false);
+  };
 
-    const handleDetailClick = (item) => {
-        setSelectedCandidate(item);
-        setIsDetailModalOpen(true);
-    };
-    const fetchGoods = async () => {
-        const res = await axios.get("https://petdom-apis.onrender.com/api/goods?search=");
-        setGoods(res.data);
-    };
-
-    React.useEffect(() => {
-        fetchGoods();
-    }, []);
-
-    return (
-        <div
-            className="StoreItem"
-            style={{
-                display: "grid",
-                gridTemplateRows: "repeat(4, minmax(0, 1fr))",
-                columnGap: "1.5rem",
-                gridTemplateColumns: "repeat(4, minmax(0, 1fr))",
-            }}
-        >
-            {goods &&
-                goods.map((item) => (
-                    <div style={{ margin: "10px 0 50px 0" }}>
-                        <img src={item.images} alt="" />
-                        <p className="item-name" onClick={() => handleDetailClick(item)} style={{ cursor: "pointer" }}>
-                            {item.name}
-                        </p>
-                        <p className="item-name" style={{ color: "#0F60DA" }}>
-                            {item.price}
-                        </p>
-                        <span style={{ fontSize: "12px", color: "#888484" }}>{item?.user?.fullname}</span>
-                    </div>
-                ))}
-            <GoodDetailModal isOpen={isDetailModalOpen} handleCloseDetailModal={handleCloseDetailModal} item={selectedCandidate} />
-        </div>
+  const handleDetailClick = (item) => {
+    setSelectedCandidate(item);
+    setIsDetailModalOpen(true);
+  };
+  const fetchGoods = async () => {
+    const res = await axios.get(
+      "https://petdom-apis.onrender.com/api/goods?search="
     );
+    setGoods(res.data);
+  };
+
+  React.useEffect(() => {
+    fetchGoods();
+  }, []);
+
+  return (
+    <div
+      className="StoreItem"
+      style={{
+        display: "grid",
+        gridTemplateRows: "repeat(4, minmax(0, 1fr))",
+        columnGap: "1.5rem",
+        gridTemplateColumns: "repeat(4, minmax(0, 1fr))",
+      }}
+    >
+      {goods &&
+        goods.map((item) => (
+          <div style={{ margin: "10px 0 50px 0" }}>
+            <img
+              src={item.images}
+              alt=""
+              style={{
+                border: "1px solid wheat",
+                borderRadius: "8px",
+                width: "200px",
+                height: "178px",
+              }}
+            />
+            <p
+              className="item-name"
+              onClick={() => handleDetailClick(item)}
+              style={{
+                cursor: "pointer",
+                display: "flex",
+                // alignItems: "center",
+                // justifyContent: "center",
+                // margin: "15px 55px 10px 15px",
+                fontSize: "17px",
+                fontWeight: "800",
+              }}
+            >
+              {item.name}
+            </p>
+            <p className="item-name" style={{ color: "#0F60DA" }}>
+              {item.price}
+            </p>
+            <span style={{ fontSize: "12px", color: "#888484" }}>
+              {item?.user?.fullname}
+            </span>
+          </div>
+        ))}
+      <GoodDetailModal
+        isOpen={isDetailModalOpen}
+        handleCloseDetailModal={handleCloseDetailModal}
+        item={selectedCandidate}
+      />
+    </div>
+  );
 }
 
 export default StoreItem;
